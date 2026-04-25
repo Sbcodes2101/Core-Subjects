bc10f78632a08415b5b82730dc4e0aa9308752da
Sbcodes2101 <sarthakbahuguna7269@gmail.com>
2026-04-25 10:20:36 +0530

    constructor and it's type

---
+++ OOPs/Constructor and destructor/constructorDestructor.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Customer{
+    string name;
+    int account_number;
+    int balance;
+    int *roi;
+    public:
+    // Default Constructor
+    Customer(){
+        cout<<"Hello constructor"<<endl;
+        name="Sarthak";
+        account_number = 123;
+        balance = 42;
+        roi = new int(100);
+    }
+
+    // Parameterized Constuctor
+    // Customer(string name,int account_number,int balance){
+    //     this->name = name;
+    //     this->account_number=account_number;
+    //     this->balance=balance;
+    // }
+    Customer(string name,int account_number){
+        this->name = name;
+        this->account_number=account_number;
+    }
+
+    // Inline Constructor
+    inline Customer(string a,int b,int c): name(a),account_number(b),balance(c){}
+    void display(){
+        cout<<name<<" "<<account_number<<" "<<balance<<endl;
+    }
+
+    Customer(Customer &B)
+    {
+        name = B.name;
+        account_number = B.account_number;
+        balance = B.balance;
+    }
+};
+
+int main(){
+    Customer A1;
+    Customer A2("Sarthak",143,42); //file open
+    Customer A3("Samay",143);
+    A1.display();
+    A2.display();
+    A3.display();
+    Customer A4(A3);
+    A4.display();
+}
---
+++ OOPs/Constructor and destructor/constructoranddestructor.md
@@ -0,0 +1,14 @@
+# CONSTRUCTOR
+- It is a special function invoked automatically at the time of object creation
+
+-Name of constructor should be same as that off class
+
+-It does not have any return value
+
+-It is used to initialize the value
+
+This pointer
+It helps differentiate class data members from local variables with the same name.
+
+-default constructor is used to initialize pointer 
+Without this, the pointer would hold a garbage value, which can lead to undefined behavior if you accidentally dereference it.
